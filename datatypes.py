28c8bb3f0b5eb8ec37c5e71379a46e599077aa1b
25Madhav <madhavmakwani@gmail.com>
2025-04-02 17:01:28 +0100

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+# pizza="Madhav"
+# print(pizza)
+# print(type(pizza))
+# oink=76767
+# print(type(oink))
+# humans=1.565747848349
+# print(humans)
+# print(type(humans))
+# mik=True
+# print(mik)
+# print(type(mik))
+# a=int(input("enter number"))
+# b=int(input("enter number"))
+# print(type(a))
+# print(type(b))
+# print(a+b)
+# kino=str(7489)
+# print(type(kino))
+tui="Madhav" 
+iut="Makwani" 
+print(tui+iut)     #concatenation 
+print(len(tui))
+print(tui[3]) 
+print(tui[1:5])    
