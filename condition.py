e41f9f562083a4fdfa62a86b0d109c6ab3ee9249
25Madhav <madhavmakwani@gmail.com>
2025-04-09 10:14:20 +0100

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,10 @@
+a=int(input("enter number:"))
+if a > 0:
+    print("number is positive.")
+else:
+    print("number is negetive.")
+age=int(input("what is your age:"))
+if age> 17:
+    print("you are eligible to vote.")
+else:
+    print("you can't vote yet.")
---
+++ profit and loss.py
@@ -0,0 +1,7 @@
+price=100
+sellingprice=120
+if price < sellingprice:
+    print("you are making a profit of")
+    print(sellingprice-price)
+else:
+    print("you are making a loss.")
