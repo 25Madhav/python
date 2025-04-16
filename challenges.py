3358466b162933b4981e343f7b055008df6fb109
25Madhav <madhavmakwani@gmail.com>
2025-04-16 10:12:44 +0100

    Add files via upload

---
+++ challenges.py
@@ -0,0 +1,34 @@
+# a=int(input("enter number number:"))
+# if a%7==0:
+#     print("the number is divisible by 7.")
+# else:
+#     print("the number is not divisible by 7.")
+
+# mean=38
+# wrongnumber=36
+# realnumber=56
+# sum=mean*40
+# print(sum)
+# num=wrongnumber-realnumber
+# ber=(sum-num)/40
+# print(ber)
+
+# a=int(input("enter number:"))
+# b=int(input("enter number:"))
+# c=int(input("enter number:"))
+# avg=(a+b+c)/3
+# if a<avg:
+#     print("a is riding below average.")
+# elif b<avg:
+#     print("b is riding below average.")
+# else:
+#     print("c is riding below average.")
+
+# a=int(input("enter number:"))
+# if a%3==0 and a%5==0:
+#     print("the number is divisible by 3 and 5.")
+# else:
+#     print("the number is not divisible by 3 and 5.")
+a=7578967769763476486784674563666663
+sum=a%10
+print(sum)
