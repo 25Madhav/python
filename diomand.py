86923f2a112de1c6beea48f719225cae2b9c6df8
25Madhav <madhavmakwani@gmail.com>
2025-05-20 17:37:24 +0100

    Add files via upload

---
+++ diomand.py
@@ -0,0 +1,10 @@
+for a in range(1,6):
+    for b in range(6,a,-1):
+        print(" ",end=" ")
+    for c in range(1,a):
+        print("*",end=" ")
+    print("")
+for d in range(1,6):
+    for e in range(6,a):
+        print("*",end=" ")
+    print("")
