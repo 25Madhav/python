75cfa232f27427877aa4b7caf9379f5cda765af2
25Madhav <madhavmakwani@gmail.com>
2025-06-20 16:50:30 +0100

    Add files via upload

---
+++ tple_practice.py
@@ -0,0 +1,19 @@
+# a=(1,2,3)
+# print(a[1])
+# b=(4,5,6)
+# c=list(b)
+# c[1]=8
+# b=tuple(c)
+# print(b)
+# d=(7,8,5)
+# (5,1,2)=d
+# print(5)
+# print(1)
+# print(2)
+# e=(3,7,1)
+# for f in e:
+#     print(f)
+t1=(6,2,8)
+t2=(9,4,5)
+t3=t1+t2
+print(t3)
