a3444166757b46aff8c55d434c2838f180b7f23d
25Madhav <madhavmakwani@gmail.com>
2025-08-08 14:47:04 +0100

    Add files via upload

---
+++ roman_numerals.py
@@ -0,0 +1,21 @@
+a=int(input("enter a number from 1-10:"))
+if a==1:
+    print("1 in roman numerals is I.")
+elif a==2:
+    print("2 in roman numerals is II.")
+elif a==3:
+    print("3 in roman numerals is III.")
+elif a==4:
+    print("4 in roman numerals is IV")
+elif a==5:
+    print("5 in roman numerals is V.")
+elif a==6:
+    print("6 in roman numerals is VI.")
+elif a==7:
+    print("7 in roman numerals is VII")
+elif a==8:
+    print("8 in roman numerals is VIII.")
+elif a==9:
+    print("9 in roman numerals is IX.")
+elif a==10:
+    print("10 in roman numerals is X")
